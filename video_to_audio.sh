22839189cda2f99d8db9c94a24b75c74ddf7fcf7
vinno <218386044+vinn-o@users.noreply.github.com>
2026-09-21 12:48:14 +0300

    comments for running the script properly

--- video_to_audio.sh
+++ video_to_audio.sh
@@ -1,2 +1,7 @@
+#!/usr/bin/bash
 #install ffmpeg first
-ffmpeg -i input.mp4 -vn -acodec libmp3lame -q:a 2 output.mp3
+#for simplicity place the  .mp4 file in the same location as the script
+#replace input with the file name to be converted
+#you can change the output name to whichever name
+# run the script with   bash video_to_audio.sh
+ffmpeg -i TokyoGabber.mp4 -vn -acodec libmp3lame -q:a 2 output3.mp3
